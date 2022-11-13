9b8f28f197a36eeb71c9b93d69180dc788d00909
goldia <goldia2314@gmail.com>
2022-11-13 16:58:43 +0700

    cau 1 2

---
+++ jedis-test/src/main/java/app/JedisPub.java
@@ -0,0 +1,14 @@
+package app;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class JedisPub {
+	
+	public static void main(String[] args) {
+		JedisPool jedisPool = new JedisPool("127.0.0.1");
+		Jedis jPublisher = new Jedis();
+		jPublisher.publish("channel", "test message");
+	}
+
+}
